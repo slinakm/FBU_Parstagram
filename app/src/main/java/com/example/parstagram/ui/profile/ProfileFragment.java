1419f080fa742598019c8ed5ddf0b57dfe80d033
Selina Kim <selinakim@fb.com>
2020-07-07 18:08:45 -0700

    Finished log out button: now goes to login activity

--- app/src/main/java/com/example/parstagram/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/parstagram/ui/profile/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.parstagram.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,24 +14,46 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.parstagram.LoginActivity;
 import com.example.parstagram.R;
+import com.example.parstagram.databinding.FragmentProfileBinding;
+import com.example.parstagram.ui.home.HomeFragment;
+import com.example.parstagram.ui.home.HomeViewModel;
+import com.parse.ParseUser;
 
 public class ProfileFragment extends Fragment {
+    private static final String TAG = ProfileFragment.class.getSimpleName();
 
     private ProfileViewModel notificationsViewModel;
+    private FragmentProfileBinding binding;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.activity_signup, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        binding = FragmentProfileBinding.inflate(getLayoutInflater());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnLogin.setOnClickListener(new logoutOnClickListener());
+    }
+
+    private class logoutOnClickListener implements View.OnClickListener {
+        public void onClick(View view) {
+            Log.i(TAG, "onClick: submit button was clicked by user");
+
+            ParseUser.logOut();
+            ParseUser currentUser = ParseUser.getCurrentUser();
+
+            Intent i = new Intent(ProfileFragment.this.getContext(), LoginActivity.class);
+            getContext().startActivity(i);
+        }
     }
 }
